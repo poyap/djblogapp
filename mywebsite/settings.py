7eaf2afca501d567f5929c390d0c4f9a7f1c6d4e
pouya <p.pouya74@gmail.com>
2021-08-30 18:02:55 +0430

    edited settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'mywebsite',
     'crispy_forms',
     'taggit',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 MIDDLEWARE = [
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mywebsite.urls'
