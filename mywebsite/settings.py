17bcdf2c3dae673c721367fc8304170c6ccdbb7b
pouya <p.pouya74@gmail.com>
2021-08-30 22:09:00 +0430

    removed static_root

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -120,7 +120,7 @@ LOGIN_URL = 'articles:login'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
     ]
